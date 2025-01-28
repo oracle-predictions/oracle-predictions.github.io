21141a22e0d534528e4a98abd1e8e023f452dfa5
Aleksandr Cheprasov <cheprasov.dev@gmail.com>
2025-01-28 08:19:14 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Переменные
+APP_DIR="../app"
+BUILD_DIR="./app"
+DIST_DIR="./app/dist"
+TARGET_DIR="."
+
+# Копируем приложение
+cp -r $APP_DIR $BUILD_DIR
+
+# Переходим в директорию приложения
+cd $BUILD_DIR || exit
+
+# Выполняем сборку приложения
+npm run build:prod:tg
+
+# Возвращаемся в предыдущую директорию
+cd .. || exit
+
+# Копируем файлы из папки dist в целевую директорию
+cp -r $DIST_DIR/* $TARGET_DIR
+
+# Добавляем изменения в git
+git add .
+
+# Создаем коммит с сообщением "deploy"
+git commit -m "deploy"
+
+# Отправляем изменения в удаленный репозиторий
+git push
+
+# Уведомление об успешном выполнении скрипта
+echo "Deploy script executed successfully."
